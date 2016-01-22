c93cf84636d45ad1ad23a6060a44d5e32b348914
Aaron Levine <aglevine@wisc.edu>
2016-01-22 13:17:33 -0600

    Use |Eta| not Eta in muon corections

--- lfvmutau/run.sh
+++ lfvmutau/run.sh
@@ -3,18 +3,72 @@
 set -o nounset
 set -o errexit
 
-#export jobid=SubmitMuTauSingleMU
-#export jobid=MuTauSingleMUV6
-#export jobid=MuTauSingleMuJetReReco
-#export jobid=MuTauSingleMuJetReReco
-export jobid=MuTauSingleMuJetReRecoSkim
-export PU=true
-rake mttight
-export PU=false
-rake pu1mttight
-export jobid=MuTauSingleMuJetReReco
-export jobidPU=MuTauSingleMuJetReReco
+python MakeSysAnalyzers.py jesup
+python MakeSysAnalyzers.py jesdown
+python MakeSysAnalyzers.py uesup
+python MakeSysAnalyzers.py uesdown
 
-rake analyzeMuTauData
-rake analyzeMuTauMC
+export systematic=jesup
+export jobid=MiniAodV2For25ns_ExtraJets_JesUes_LFV
+export isRealData=false
+export isZTauTau=false
+#rake analyzeSpring2015MiscJesUp
+#rake analyzeSpring2015WJetsJesUp
+#rake analyzeSpring2015ZJetsJesUp
+export isZTauTau=true
+#rake analyzeSpring2015ZTauTauJetsJesUp
+export isZTauTau=false
+export isRealData=true
+#rake analyzeLFVMuTauDataJesUp
 
+export systematic=jesdown
+export jobid=MiniAodV2For25ns_ExtraJets_JesUes_LFV
+export isRealData=false
+export isZTauTau=false
+#rake analyzeSpring2015MiscJesDown
+#rake analyzeSpring2015WJetsJesDown
+#rake analyzeSpring2015ZJetsJesDown
+export isZTauTau=true
+#rake analyzeSpring2015ZTauTauJetsJesDown
+export isZTauTau=false
+export isRealData=true
+#rake analyzeLFVMuTauDataJesDown
+
+export systematic=uesup
+export jobid=MiniAodV2For25ns_ExtraJets_JesUes_LFV
+export isRealData=false
+export isZTauTau=false
+#rake analyzeSpring2015MiscUesUp
+#rake analyzeSpring2015WJetsUesUp
+#rake analyzeSpring2015ZJetsUesUp
+export isZTauTau=true
+#rake analyzeSpring2015ZTauTauJetsUesUp
+export isZTauTau=false
+export isRealData=true
+#rake analyzeLFVMuTauDataUesUp
+
+export systematic=uesdown
+export jobid=MiniAodV2For25ns_ExtraJets_JesUes_LFV
+export isRealData=false
+export isZTauTau=false
+#rake analyzeSpring2015MiscUesDown
+#rake analyzeSpring2015WJetsUesDown
+#rake analyzeSpring2015ZJetsUesDown
+export isZTauTau=true
+#rake analyzeSpring2015ZTauTauJetsUesDown
+export isZTauTau=false
+export isRealData=true
+#rake analyzeLFVMuTauDataUesDown
+
+export systematic=none
+export jobid=MiniAodV2For25ns_ExtraJets_JesUes_JetEtaFix_LFV
+export isRealData=false
+export isZTauTau=false
+rake analyzeSpring2015Misc
+rake analyzeSpring2015WJets
+rake analyzeSpring2015ZJets
+export isZTauTau=true
+rake analyzeSpring2015ZTauTauJets
+export isZTauTau=false
+export isRealData=true
+rake analyzeLFVMuTauData
--- lfvmutau/setup.sh
+++ lfvmutau/setup.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
 
 # Get the data
-export datasrc=/scratch/aglevine/data
-#export datasrc=/scratch/mcepeda/data
-
-export jobid=MuTauSingleMuJetReReco
-export jobid8TeV=$jobid
+export datasrc=/hdfs/store/user/aglevine/
+#export datasrc=/hdfs/store/user/cepeda/
+#export datasrc=/hdfs/store/user/taroni/
+#export jobid=MiniAODSIM-Spring15-25ns_LFV_V1_October10
+#export jobid=MiniAODSIM-Spring15-25ns_LFV_MiniAODV2_Nov3
+#export jobid=MiniAODSIMv2-Spring15-25ns_LFV_October13
+#export jobid=MiniAODv2_2fb_v3
+#export jobid=MiniAODSIM-Spring15-25ns_LFV_MiniAODV2_Dec2_LFV_NoHF_JetEta25
+#export jobid=MiniAODSIM-Spring15-25ns_LFV_MiniAODV2_Dec2_Data_NoHF_JetEta
+#export jobid=MiniAODSIM-Spring15-25ns_LFV_MiniAODV2_Dec2_LFV_NoHF_JetEta25_MissingHiggs
+#export jobid=MiniAodV2For25ns_ExtraJets_LFV_Data
+export jobid=MiniAodV2For25ns_ExtraJets_JesUes_JetEtaFix_LFV
+#export jobid=MiniAodV2For25ns_ExtraJets_JesUes_JetFix_LFVData
+#export jobid=MiniAODv2_2fb_v2
+#export jobid=MiniAODSIM-Spring15-25ns_LFV_MiniAODV2_Data
+#export jobid=MiniAODSIM-Spring15-25ns_LFV_Nov9_ZTTFakeStudy
+#export jobid=MiniAODSIM-Spring15-25ns_LFV_Oct27
 export afile=`find $datasrc/$jobid | grep root | head -n 1`
 
 ## Build the cython wrappers
 rake "make_wrapper[$afile, mt/final/Ntuple, MuTauTree]"
 
-echo 'Note that part of the data is in login06 (under mcepeda and aglevine) and part in login05 (under aglevine)'
-
 ls *pyx | sed "s|pyx|so|" | xargs rake 
+#echo "finishing compilation" 
+#bash compileTree.txt
 
-rake "meta:getinputs[$jobid, $datasrc,mt/metaInfo]"
-rake "meta:getmeta[inputs/$jobid, mt/metaInfo, 8]"
-
-
+rake "meta:getinputs[$jobid, $datasrc,mt/metaInfo, mt/summedWeights]"
+rake "meta:getmeta[inputs/$jobid, mt/metaInfo, 13]"
